8146c465d243d3208f8d8b1bcbd7ba203d96ab12
luisarcher <luismcjordao@hotmail.com>
2016-12-18 16:40:53 +0000

    va ok, ola

--- src/DirectoryService/ServerEntry.java
+++ src/DirectoryService/ServerEntry.java
@@ -1,11 +1,13 @@
 package DirectoryService;
 
 import FileServer.ServerHeartbeat;
+import java.net.InetAddress;
 import java.util.ArrayList;
 
 public class ServerEntry extends common.Entry{
     
     private ArrayList<String> connectedClients;
+    private InetAddress serverAddr;
     
     public ServerEntry(ServerHeartbeat hb){
         // Cria uma entrada de registo selando-a com o tempo actual.
